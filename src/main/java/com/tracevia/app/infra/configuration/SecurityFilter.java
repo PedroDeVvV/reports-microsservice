546f2d2c0ee0eda8833dbcfbaa443927ea23cebf
PedroDeVvV <pedrohgs2004@gmail.com>
2025-02-18 08:49:51 -0300

    Docker alteration test

--- src/main/java/com/tracevia/app/infra/configuration/SecurityFilter.java
+++ src/main/java/com/tracevia/app/infra/configuration/SecurityFilter.java
@@ -26,7 +26,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("calling filter");
+        System.out.println("calling filter v2");
         var tokenJWT = recoveryToken(request);
 
         if (tokenJWT != null) {
